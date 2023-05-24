159e1425e587c9d3f5e00a86cd41b1f7ba8341fd
Hast11 <andres2000gil@gmail.com>
2023-05-24 15:43:48 +0200

    cambios migraciones

--- migrations/20230524131637-AddAuthorToPostTable.js
+++ migrations/20230524131637-AddAuthorToPostTable.js
@@ -2,34 +2,23 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-    await queryInterface.addColumn(
-      'Posts', //TABLA QUE VOY A MODIFICAR
-      'authorId', //COLUMNA QUE AÑADO
-      {
-          type: Sequelize.INTEGER,
-          references: { //A que hace referencia
-              model: "Users", // A la tabla de usuario
-              key: "id" //A traves de su id
-          },
-          onUpdate: 'CASCADE',
-          onDelete: 'SET NULL'
-      });
+  up: async (queryInterface, Sequelize) => {
+      await queryInterface.addColumn(
+          'Posts',//Tabla que voy a modificar
+          'authorId', //Columna que añado
+          {
+              type: Sequelize.INTEGER,
+              references: { //A que hace referencia
+                  model: "Users", //A la tabla de usuario
+                  key: "id" //A traves de su id
+              },
+              onUpdate: 'CASCADE',
+              onDelete: 'SET NULL'
+          }
+      );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-    await queryInterface.removeColumn('Posts', 'authorId');
+  down: async (queryInterface, Sequelize) => {
+      await queryInterface.removeColumn('Posts', 'authorId');
   }
 };
